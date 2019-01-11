3813182358db93bf2a3af84be1bf704b95f619a7
Lotanna <sugabelly@gmail.com>
2019-01-11 12:30:41 -0500

    animation
    added logic to animate the padlock unlocking and made appearance changes to view

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -13,13 +13,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        specificAppearance()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//Outlets
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var padlockView: UIImageView!
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    
+    
+ //
+    func specificAppearance() {
+        
+        bgView.layer.cornerRadius = 20
+        bgView.clipsToBounds = true
+        bgView.backgroundColor = UIColor(red: 249/255, green: 188/255, blue: 19/255, alpha: 1.0) // Mikado Yellow #F9BC13
     }
+    
+//
+    
+    let lockedImage = UIImage(named: "Locked")!
+    let unlockedImage = UIImage(named: "Unlocked")!
+    var animationArray: [UIImage] = []
 
-
+    
+    func createArray() {
+        animationArray.append(lockedImage)
+        animationArray.append(unlockedImage)
+    }
+    
+    func animateUnlock(imageView: UIImageView, images: [UIImage]) {
+        createArray()
+        imageView.animationImages = images
+        imageView.animationDuration = 1.0
+        //imageView.animationRepeatCount = 1
+        imageView.startAnimating()
+    }
+    
+    
+    
+    
+    
 }
 
